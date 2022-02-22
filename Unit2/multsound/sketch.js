a724ad301bb2944280ed1abfbe6899a232205545
Rose <31122139+rosepkid@users.noreply.github.com>
2022-02-22 11:35:16 -0600

    test

---
+++ Unit2/multsound/sketch.js
@@ -0,0 +1,53 @@
+let state = 0;
+let song1, song2, song3;
+
+function preload() {
+  song1 = loadSound("assets/s1.mp3");
+  song2 = loadSound("assets/s2.mp3");
+  song3 = loadSound("assets/s3.mp3");
+}
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+
+  switch (state) {
+    case 0:
+    song1.play() ;
+    state = 1 ;
+      break;
+
+    case 1:
+    background('red') ;
+    text("listening to song 1", 100, 100) ;
+      break;
+
+    case 2:
+    song2.play() ;
+    state = 3 ;
+      break;
+
+    case 3:
+    background('green') ;
+      break;
+
+    case 4:
+    song3.play() ;
+    state = 5;
+      break;
+
+    case 5:
+      background('yellow') ;
+      break;
+
+  }
+}
+
+function mouseReleased() {
+  state = (state + 1) % 5 ;
+  song1.pause() ;
+  song2.pause() ;
+  song3.pause() ;
+}
